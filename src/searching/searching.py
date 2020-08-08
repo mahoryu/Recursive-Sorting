cce60699e9a5c71746dcd91d0b5fe6dc0a7f3fa6
mahoryu <mahoryu@gmail.com>
2020-08-08 11:22:26 -0600

    Finished and passed the sorting file including an edge case that wasn't originally in the test file

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,7 +1,22 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
     # Your code here
-
+    mid = ((start + end) // 2)
+    if mid == start and start != end:
+        mid += 1
+    if arr == []:
+        return -1   
+    elif arr[mid] == target:
+        return mid
+    elif start == end:
+        return -1
+    else:
+        if target < arr[mid]:
+            found = binary_search(arr[:mid], target, start, mid-1)
+        else:
+            found = binary_search(arr[mid:], target, mid-mid, end-mid)
+    return found
+    
 
 # STRETCH: implement an order-agnostic binary search
 # This version of binary search should correctly find 
@@ -11,4 +26,4 @@ def binary_search(arr, target, start, end):
 # or iteratively
 def agnostic_binary_search(arr, target):
     # Your code here
-
+    pass
