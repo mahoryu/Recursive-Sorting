ad156070144f561762e0d97e89bbd72a894cdff2
mahoryu <mahoryu@gmail.com>
2020-08-08 12:25:56 -0600

    Passed the test for the agnostic binary search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -26,4 +26,29 @@ def binary_search(arr, target, start, end):
 # or iteratively
 def agnostic_binary_search(arr, target):
     # Your code here
-    pass
+    if arr[0] <= arr[-1]:
+        ascending = True
+    else:
+        ascending = False
+    beg_i = 0
+    end_i = len(arr) - 1
+    
+    found = False
+    while beg_i <= end_i and not found:
+        mid_i = (beg_i + end_i) // 2
+        
+        if arr[mid_i] == target:
+            found = True
+            return mid_i
+        elif ascending:
+            if target < arr[mid_i]:
+                end_i = mid_i - 1
+            else:
+                beg_i = mid_i + 1
+        else:
+            if target > arr[mid_i]:
+                end_i = mid_i - 1
+            else:
+                beg_i = mid_i + 1
+
+    return -1  # not found
